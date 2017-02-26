8ff6ed6333af5b35bbd5914b45350113ed807950
Ian Edwards <ian@myacorn.com>
2017-02-26 10:15:02 +0000

    - Remove splash screen (cause Invoke error on Win 7) - Added playback images, licence file and setup script

--- src/StarPlayerPC/My Project/Application.Designer.vb
+++ src/StarPlayerPC/My Project/Application.Designer.vb
@@ -34,10 +34,5 @@ Namespace My
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.StarPlayerPC.frmEditor
         End Sub
-        
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
-        Protected Overrides Sub OnCreateSplashScreen()
-            Me.SplashScreen = Global.StarPlayerPC.frmSplash
-        End Sub
     End Class
 End Namespace
